345950b562f33344cb573be9282ffc6e3f20448c
RafaelBAraujo <barbaroto96@gmail.com>
2019-06-07 10:33:22 -0300

    Post atoms push

---
+++ client/src/components/atoms/PostContent.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PostContent = ({content}) => {
+    return(
+        <div className="comment-content">
+            {content}
+        </div>
+    )
+}
+
+export default PostContent
---
+++ client/src/components/atoms/PostTitle.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PostTitle = ({title}) => {
+    return(
+        <div className="post-title">
+            <p>{title}</p>
+        </div>
+    )
+}
+
+export default PostTitle
