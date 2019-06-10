edc36169e9ed4fd82161e966b52470506e25ecd3
brian_wilson66 <barbaroto96@gmail.com>
2019-06-10 19:52:59 -0300

    Search engine draft

--- client/src/components/scripts.js
+++ client/src/components/scripts.js
@@ -38,8 +38,28 @@ function switchView(view) {
     
 }
 
+var Mark = require('mark.js')
+
+const options = { "element": "mark", "className": "", "exclude": [], "separateWordSearch": true, "accuracy": "partially", "diacritics": true, "synonyms": {}, "iframes": false, "iframesTimeout": 5000, "acrossElements": false, "caseSensitive": false, "ignoreJoiners": false, "ignorePunctuation": [], "wildcards": "disabled", "each": function(node){}, "filter": function(textNode, foundTerm, totalCounter, counter){ return true; }, "noMatch": function(term){}, "done": function(counter){}, "debug": false, "log": window.console }
+
 export const search = (input) => {
 
-    //new Mark(document.querySelector('.context')).mark('Lorem');
+    let keywords = input.target.value.split(' ')
 
+    let contexts = document.getElementsByClassName('comment-content')
+
+    for(let i = 0; i < contexts.length; ++i) {
+        let context = contexts[i]
+        let instance = new Mark(context)
+        instance.unmark(options)
+        instance.mark(keywords, options)
+    }
+    /*
+    contexts.forEach(context => {
+        let instance = new Mark(context)
+        instance.unmark(options)
+        instance.mark(keywords, options)
+    })
+    */
 }
+
