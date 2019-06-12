34af80d3a7cb7a4c89a82915e511c20934914c3e
brian_wilson66 <barbaroto96@gmail.com>
2019-06-11 21:43:42 -0300

    Comment filter

--- client/src/components/atoms/CommentRibbon.jsx
+++ client/src/components/atoms/CommentRibbon.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 const CommentRibbon = ({}) => {
     return (
         <div className="ribbon">
+            
         </div>
     )
 }
--- client/src/components/molecules/Node.jsx
+++ client/src/components/molecules/Node.jsx
@@ -4,9 +4,11 @@ import NodeText from '../atoms/NodeText'
 import NodeValue from '../atoms/NodeValue'
 import RoundPicture from '../atoms/RoundPicture'
 
+import { filterCommentsByAuthor } from '../scripts'
+
 const Node = ({imgSrc, imgAlt, text, value}) => {
     return(
-        <div className="node">
+        <div className="node" onClick={() => filterCommentsByAuthor(text)} >
             <RoundPicture src={imgSrc} alt={imgAlt}/>
             <NodeText text={text} />
             <NodeValue value={value} />
