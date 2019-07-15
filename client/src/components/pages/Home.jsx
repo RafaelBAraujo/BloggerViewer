46e9e82552ff563d04826c0250695b27fc4bca66
brian_wilson66 <barbaroto96@gmail.com>
2019-07-15 00:29:28 -0300

    Minor update to Home page style

--- client/src/components/pages/Home.jsx
+++ client/src/components/pages/Home.jsx
@@ -4,28 +4,45 @@ import { Link } from 'react-router-dom'
 import '../../stylesheets/home.css'
 
 class Home extends Component {
-  render() {
-    return (
-    <div className="App">
-    
-        <h1 className="welcome-text">Sala de Aula 360 Graus</h1>
 
-        <div className="blog-links">
-          <Link to={'./visualizer/adm_tec'}>
-              <button className="blog" >Administração - TEC</button>
-          </Link>
+	doSomething = () => {
+		document.getElementById('crap').classList.toggle('hide')
 
-          <Link to={'./visualizer/adm_si'}>
-              <button className="blog" >Administração - SI</button>
-          </Link>
+		document.getElementById('crap2').classList.toggle('hidden')
+	}
 
-          <Link to={'./visualizer/tgs'}>
-              <button className="blog" >Teoria Geral de Sistemas</button>
-          </Link>
-        </div>
+	render() {
+		return (
+			<div className="App">
+			
+				<div className="home-wrapper overlay">
 
-    </div>
-    );
-  }
+					<h1 className="welcome-text">Sala de Aula 360 Graus</h1>
+
+						<div className="home-menu">
+							<button id="crap" className="home-menu-btn" type="button" onClick={() => this.doSomething()}>
+								Visualizar um blog
+							</button>
+						</div>
+						<div id="crap2" className="blog-links hidden">
+							<Link to={'./visualizer/adm_tec'}>
+								<button className="blog" >Administração - TEC</button>
+							</Link>
+
+							<Link to={'./visualizer/adm_si'}>
+								<button className="blog" >Administração - SI</button>
+							</Link>
+
+							<Link to={'./visualizer/tgs'}>
+								<button className="blog" >Teoria Geral de Sistemas</button>
+							</Link>
+						</div>
+
+				</div>
+				
+
+			</div>
+		);
+	}
 }
 export default Home;
