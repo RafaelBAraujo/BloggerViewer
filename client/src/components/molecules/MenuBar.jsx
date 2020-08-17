8d57e1dd8df3c839abac35cb6076087620a8287f
Rafael Barbaroto <rafaraujo@deloitte.com>
2020-08-17 04:46:59 -0300

    removing unused cmps

--- client/src/components/molecules/MenuBar.jsx
+++ client/src/components/molecules/MenuBar.jsx
@@ -3,8 +3,6 @@ import React from 'react'
 import Tab from '../atoms/Tab'
 import SearchBar from '../atoms/SearchBar'
 import DrawerButton from '../atoms/DrawerButton'
-import ConfigButton from '../atoms/ConfigButton'
-import ReturnButton from '../atoms/ReturnButton'
 
 const MenuBar = ({color}) => {
     
